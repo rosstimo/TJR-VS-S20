e376c7fd9b823c7ea7ff781ab03cf8e0bfa1d10c
Tim Rossiter <rosstimo@isu.edu>
2020-01-10 05:46:30 -0700

    Add Arithmetic Operators

---
+++ Arithmetic Operators/ArithmeticOperators.vb
@@ -0,0 +1,48 @@
+﻿Public Module ArithmeticOperators
+
+    Public Sub Main()
+
+        Console.WriteLine("Adding 2 + 2")
+        Console.WriteLine(2 + 2)
+
+        Console.ReadLine()
+
+        Console.WriteLine("Subtracting 6 - 4")
+        Console.WriteLine(6 - 4)
+
+        Console.ReadLine()
+
+        Console.WriteLine("3 x 4 is")
+        Console.WriteLine(3 * 4)
+
+        Console.ReadLine()
+
+        Console.WriteLine("Floating Point Division")
+        Console.WriteLine("20 / 6 is")
+        Console.WriteLine(20 / 6)
+
+        Console.ReadLine()
+
+        Console.WriteLine("Integer Division")
+        Console.WriteLine("20 \ 6 is")
+        Console.WriteLine(20 \ 6)
+
+        Console.ReadLine()
+
+        Console.WriteLine("Modulus gives only the remainder")
+        Console.WriteLine("20 Mod 6 is")
+        Console.WriteLine(20 Mod 6)
+
+        Console.ReadLine()
+
+        Console.WriteLine("5^2 is")
+        Console.WriteLine(5 ^ 2)
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
+
+
+
