1c5e3ead3383c740e8157c0c5e22b856377e764e
Tim Rossiter <rosstimo@isu.edu>
2020-01-15 05:49:27 -0700

    add ArithmeticOperators example

---
+++ HelloWorld-Class/Module1.vb
@@ -0,0 +1,13 @@
+﻿'Tim Rossiter
+'RCET0265
+'Spring 2020
+'Hello World
+
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("Hello, World!")
+        Console.ReadLine()
+    End Sub
+
+End Module
