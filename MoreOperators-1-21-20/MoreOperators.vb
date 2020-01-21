efaada8f6d0ddfc3cf4d2da30a78667e22ecb810
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-01-21 08:55:25 -0700

    add user message prompt

---
+++ MoreOperators-1-21-20/MoreOperators.vb
@@ -0,0 +1,20 @@
+﻿Option Explicit On
+Option Strict On
+
+Module MoreOperators
+
+    Sub Main()
+        'promt user for a message
+        'then display the message
+        Dim userMessage As String
+
+        'userMessage = "Hello"
+
+        Console.WriteLine("Please enter a message. Press enter when done.")
+        userMessage = Console.ReadLine()
+        Console.WriteLine("You entered " & Chr(34) & userMessage & Chr(34) & " Good job!")
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ Variables1-16-20/VariableExample.vb
@@ -0,0 +1,27 @@
+﻿Option Strict On
+
+'Tim Rossiter
+'RCET0265
+'Spring 2020
+'Variable Example
+
+'Module names PascalCase
+Module VariableExample
+
+    'Method names PascalCase
+    Sub Main()
+
+        'name variables in camelCase
+        Dim appleCount As Integer
+        Dim fruitName As String
+
+        fruitName = "apples" '& " Banana"
+        'Console.WriteLine(fruitName)
+
+        appleCount = 4
+        Console.WriteLine("You have " & appleCount & " " & fruitName & "!")
+
+        Console.ReadLine()
+    End Sub
+
+End Module
