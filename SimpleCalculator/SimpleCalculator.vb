7bab16059e672579b3c8ea7ef47aff50dd0ba987
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-01-28 08:19:06 -0700

    fix try catch

--- SimpleCalculator/SimpleCalculator.vb
+++ SimpleCalculator/SimpleCalculator.vb
@@ -5,9 +5,6 @@
         Dim secondNumber As String
         Dim userChoice As String
 
-
-
-
         'prompt user for two numbers
         Console.WriteLine("Please enter a number...")
         firstNumber = Console.ReadLine()
@@ -24,25 +21,25 @@
 
         'Determine the user's choice
         If userChoice = "1" Then
-
             Try
                 Console.WriteLine(CInt(firstNumber) + CInt(secondNumber))
             Catch ex As Exception
                 Console.WriteLine("Please enter a whole number")
+                'Console.WriteLine(ex.Message.ToString)
             End Try
-
         ElseIf userChoice = "2" Then
+            Try
                 Console.WriteLine(CInt(firstNumber) * CInt(secondNumber))
-                Else
-                Console.WriteLine("Invalid Selection")
+            Catch ex As Exception
+                Console.WriteLine("Please enter a whole number")
+            End Try
+        Else
+            Console.WriteLine("Invalid Selection")
         End If
-
-
 
         Console.WriteLine("Have a nice day...")
         'display result prompt to end
         Console.ReadLine()
-
 
     End Sub
 
