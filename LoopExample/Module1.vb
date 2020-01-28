e0378c8836b51519e4bd10b6f61a7bfb5e004e8b
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-01-28 08:39:22 -0700

    create loop example

---
+++ LoopExample/Module1.vb
@@ -0,0 +1,27 @@
+﻿Option Explicit On
+Option Strict On
+Option Compare Binary
+Module Module1
+
+    Sub Main()
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+        Dim result As Integer
+
+        firstNumber = 5
+        secondNumber = 7
+
+        result = 0 'firstNumber + secondNumber
+
+        For i = 0 To 25 Step 2
+            result = result + 1
+            Console.WriteLine(i)
+        Next
+
+        Console.WriteLine(result)
+        Console.ReadLine()
+
+
+    End Sub
+
+End Module
