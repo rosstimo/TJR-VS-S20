ccb4a5734682500f31be3f62efde3b9a99721283
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-01-30 08:57:40 -0700

    create sub function example

---
+++ SubFuctionExample/Module1.vb
@@ -0,0 +1,44 @@
+﻿Option Strict On
+Option Explicit On
+
+Module Module1
+
+    Sub Main()
+
+        Dim myNumber As Integer
+        myNumber = 7
+        Console.WriteLine("In Sub Main")
+        Console.WriteLine(myNumber)
+        Console.ReadLine()
+
+        MySub(myNumber)
+        Console.WriteLine("In Sub Main")
+        Console.WriteLine(myNumber)
+
+        myNumber = MyFunc(myNumber)
+        Console.WriteLine("In Sub Main")
+        Console.WriteLine(myNumber)
+
+
+        Console.ReadLine()
+    End Sub
+
+    Sub MySub(myNumber As Integer)
+        'Dim myNumber As Integer
+        'myNumber = myNumber + 2
+        myNumber += 2
+        Console.WriteLine("In Sub MySub")
+        Console.WriteLine(myNumber)
+        Console.ReadLine()
+    End Sub
+
+    Function MyFunc(myNumber As Integer) As Integer
+        'Dim myNumber As Integer
+        myNumber -= 3
+        Console.WriteLine("In Function MyFunc")
+        Console.WriteLine(myNumber)
+        Console.ReadLine()
+        Return myNumber
+    End Function
+
+End Module
