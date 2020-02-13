c6c7e9deb6b79d30cf5c2269d6bf1a4b7d80bf99
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-02-13 08:54:39 -0700

    add IsStringEmpty Function

--- ValidateUserData/ValidateUserDataForm.vb
+++ ValidateUserData/ValidateUserDataForm.vb
@@ -1,25 +1,32 @@
 ﻿Public Class ValidateUserDataForm
     Private Sub ValidateButton_Click(sender As Object, e As EventArgs) Handles ValidateButton.Click
-        IsEmpty()
+        Dim userMessage As String
 
+        'IsEmpty()
+        If IsStringEmpty(TextBox3.Text) = True Then
+            userMessage &= "please enter text in textbox 3" & vbNewLine
+            TextBox3.Select()
+        End If
+
+        If IsStringEmpty(TextBox2.Text) = True Then
+            userMessage &= "please enter text in textbox 2" & vbNewLine
+            TextBox2.Select()
+        End If
+
+        If IsStringEmpty(TextBox1.Text) = True Then
+            userMessage &= "please enter text in textbox 1" & vbNewLine
+            TextBox1.Select()
+        End If
+
+        If userMessage <> "" Then
+            MsgBox(userMessage)
+        End If
 
     End Sub
 
     Sub IsEmpty()
         'Check each if empty and alert user
         Dim userMessage As String
-
-        'Another way to check a condition
-        Select Case ""
-            Case TextBox3.Text
-                userMessage &= "please enter text in textbox 3" & vbNewLine
-            Case TextBox2.Text
-                userMessage &= "please enter text in textbox 2" & vbNewLine
-            Case TextBox1.Text
-                userMessage &= "please enter text in textbox 1" & vbNewLine
-            Case Else
-
-        End Select
 
         If TextBox3.Text = "" Then
             userMessage &= "please enter text in textbox 3" & vbNewLine
@@ -36,8 +43,34 @@
             TextBox1.Select()
         End If
 
+        If userMessage <> "" Then
+            MsgBox(userMessage)
+        End If
 
-        MsgBox(userMessage)
+        'Another way to check a condition
+        'Select Case ""
+        '    Case TextBox3.Text
+        '        userMessage &= "please enter text in textbox 3" & vbNewLine
+        '    Case TextBox2.Text
+        '        userMessage &= "please enter text in textbox 2" & vbNewLine
+        '    Case TextBox1.Text
+        '        userMessage &= "please enter text in textbox 1" & vbNewLine
+        '    Case Else
+
+        'End Select
+
     End Sub
+
+    Function IsStringEmpty(stringToTest As String) As Boolean
+        Dim status As Boolean
+        If stringToTest = "" Then
+            status = True
+        Else
+            status = False
+        End If
+        Return status
+    End Function
+
+
 
 End Class
