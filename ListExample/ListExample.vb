aacd75bf2869f57d6f1d39d261a62cc39968923a
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-02-20 08:44:47 -0700

    add list example

---
+++ ListExample/ListExample.vb
@@ -0,0 +1,28 @@
+﻿Module ListExample
+
+    Sub Main()
+        Dim names As New List(Of String)
+        Dim searchString As String
+
+        names.Add("Tim")
+        names.Add("Joe")
+
+        'names.Add(Console.ReadLine())
+
+        searchString = Console.ReadLine()
+
+        If names.Contains(searchString) = True Then
+            Console.WriteLine("Yes, " & searchString & " is at index " & names.IndexOf(searchString))
+        Else
+            Console.WriteLine("Sorry, " & searchString & " was not found.")
+        End If
+
+
+        'For Each name In names
+        '    Console.WriteLine(name)
+        'Next
+
+        Console.ReadLine()
+    End Sub
+
+End Module
