cf29379fc8eda342c383769188698cffe0aed73d
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-02-24 08:54:18 -0700

    add some stuff

--- ListBoxExample/ListBoxForm.Designer.vb
+++ ListBoxExample/ListBoxForm.Designer.vb
@@ -26,21 +26,26 @@ Partial Class ListBoxForm
         Me.AddButton = New System.Windows.Forms.Button()
         Me.ClearButton = New System.Windows.Forms.Button()
         Me.ExitButton = New System.Windows.Forms.Button()
+        Me.FirstNameTextBox = New System.Windows.Forms.TextBox()
+        Me.LastNameTextBox = New System.Windows.Forms.TextBox()
+        Me.FirstNameLabel = New System.Windows.Forms.Label()
+        Me.LastNameLabel = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'DataListBox
         '
+        Me.DataListBox.Font = New System.Drawing.Font("Consolas", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.DataListBox.FormattingEnabled = True
-        Me.DataListBox.ItemHeight = 20
-        Me.DataListBox.Location = New System.Drawing.Point(149, 59)
+        Me.DataListBox.ItemHeight = 19
+        Me.DataListBox.Location = New System.Drawing.Point(149, 49)
         Me.DataListBox.Name = "DataListBox"
-        Me.DataListBox.Size = New System.Drawing.Size(618, 264)
+        Me.DataListBox.Size = New System.Drawing.Size(380, 213)
         Me.DataListBox.TabIndex = 0
         Me.DataListBox.TabStop = False
         '
         'AddButton
         '
-        Me.AddButton.Location = New System.Drawing.Point(275, 344)
+        Me.AddButton.Location = New System.Drawing.Point(260, 344)
         Me.AddButton.Name = "AddButton"
         Me.AddButton.Size = New System.Drawing.Size(160, 94)
         Me.AddButton.TabIndex = 1
@@ -49,7 +54,7 @@ Partial Class ListBoxForm
         '
         'ClearButton
         '
-        Me.ClearButton.Location = New System.Drawing.Point(441, 344)
+        Me.ClearButton.Location = New System.Drawing.Point(426, 344)
         Me.ClearButton.Name = "ClearButton"
         Me.ClearButton.Size = New System.Drawing.Size(160, 94)
         Me.ClearButton.TabIndex = 2
@@ -59,12 +64,44 @@ Partial Class ListBoxForm
         'ExitButton
         '
         Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.ExitButton.Location = New System.Drawing.Point(607, 344)
+        Me.ExitButton.Location = New System.Drawing.Point(592, 344)
         Me.ExitButton.Name = "ExitButton"
         Me.ExitButton.Size = New System.Drawing.Size(160, 94)
         Me.ExitButton.TabIndex = 3
         Me.ExitButton.Text = "E&xit"
         Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'FirstNameTextBox
+        '
+        Me.FirstNameTextBox.Location = New System.Drawing.Point(149, 12)
+        Me.FirstNameTextBox.Name = "FirstNameTextBox"
+        Me.FirstNameTextBox.Size = New System.Drawing.Size(250, 26)
+        Me.FirstNameTextBox.TabIndex = 4
+        '
+        'LastNameTextBox
+        '
+        Me.LastNameTextBox.Location = New System.Drawing.Point(502, 12)
+        Me.LastNameTextBox.Name = "LastNameTextBox"
+        Me.LastNameTextBox.Size = New System.Drawing.Size(250, 26)
+        Me.LastNameTextBox.TabIndex = 5
+        '
+        'FirstNameLabel
+        '
+        Me.FirstNameLabel.AutoSize = True
+        Me.FirstNameLabel.Location = New System.Drawing.Point(57, 12)
+        Me.FirstNameLabel.Name = "FirstNameLabel"
+        Me.FirstNameLabel.Size = New System.Drawing.Size(86, 20)
+        Me.FirstNameLabel.TabIndex = 6
+        Me.FirstNameLabel.Text = "First Name"
+        '
+        'LastNameLabel
+        '
+        Me.LastNameLabel.AutoSize = True
+        Me.LastNameLabel.Location = New System.Drawing.Point(410, 12)
+        Me.LastNameLabel.Name = "LastNameLabel"
+        Me.LastNameLabel.Size = New System.Drawing.Size(86, 20)
+        Me.LastNameLabel.TabIndex = 7
+        Me.LastNameLabel.Text = "Last Name"
         '
         'ListBoxForm
         '
@@ -73,6 +110,10 @@ Partial Class ListBoxForm
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CancelButton = Me.ExitButton
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LastNameLabel)
+        Me.Controls.Add(Me.FirstNameLabel)
+        Me.Controls.Add(Me.LastNameTextBox)
+        Me.Controls.Add(Me.FirstNameTextBox)
         Me.Controls.Add(Me.ExitButton)
         Me.Controls.Add(Me.ClearButton)
         Me.Controls.Add(Me.AddButton)
@@ -81,6 +122,7 @@ Partial Class ListBoxForm
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "List Box "
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -88,4 +130,8 @@ Partial Class ListBoxForm
     Friend WithEvents AddButton As Button
     Friend WithEvents ClearButton As Button
     Friend WithEvents ExitButton As Button
+    Friend WithEvents FirstNameTextBox As TextBox
+    Friend WithEvents LastNameTextBox As TextBox
+    Friend WithEvents FirstNameLabel As Label
+    Friend WithEvents LastNameLabel As Label
 End Class
