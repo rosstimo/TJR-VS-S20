2c40dfb4c30cf2aab732f6ec7982b1c14417ac96
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-02-25 08:59:28 -0700

    work on TODO

--- ListBoxExample/ListBoxForm.Designer.vb
+++ ListBoxExample/ListBoxForm.Designer.vb
@@ -36,27 +36,30 @@ Partial Class ListBoxForm
         '
         Me.DataListBox.Font = New System.Drawing.Font("Consolas", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.DataListBox.FormattingEnabled = True
-        Me.DataListBox.ItemHeight = 19
-        Me.DataListBox.Location = New System.Drawing.Point(149, 49)
+        Me.DataListBox.ItemHeight = 22
+        Me.DataListBox.Location = New System.Drawing.Point(182, 59)
+        Me.DataListBox.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.DataListBox.Name = "DataListBox"
-        Me.DataListBox.Size = New System.Drawing.Size(380, 213)
+        Me.DataListBox.Size = New System.Drawing.Size(737, 334)
         Me.DataListBox.TabIndex = 0
         Me.DataListBox.TabStop = False
         '
         'AddButton
         '
-        Me.AddButton.Location = New System.Drawing.Point(260, 344)
+        Me.AddButton.Location = New System.Drawing.Point(318, 413)
+        Me.AddButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.AddButton.Name = "AddButton"
-        Me.AddButton.Size = New System.Drawing.Size(160, 94)
+        Me.AddButton.Size = New System.Drawing.Size(196, 113)
         Me.AddButton.TabIndex = 1
         Me.AddButton.Text = "&Add Item"
         Me.AddButton.UseVisualStyleBackColor = True
         '
         'ClearButton
         '
-        Me.ClearButton.Location = New System.Drawing.Point(426, 344)
+        Me.ClearButton.Location = New System.Drawing.Point(521, 413)
+        Me.ClearButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.ClearButton.Name = "ClearButton"
-        Me.ClearButton.Size = New System.Drawing.Size(160, 94)
+        Me.ClearButton.Size = New System.Drawing.Size(196, 113)
         Me.ClearButton.TabIndex = 2
         Me.ClearButton.Text = "&Clear All"
         Me.ClearButton.UseVisualStyleBackColor = True
@@ -64,52 +67,57 @@ Partial Class ListBoxForm
         'ExitButton
         '
         Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.ExitButton.Location = New System.Drawing.Point(592, 344)
+        Me.ExitButton.Location = New System.Drawing.Point(724, 413)
+        Me.ExitButton.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.ExitButton.Name = "ExitButton"
-        Me.ExitButton.Size = New System.Drawing.Size(160, 94)
+        Me.ExitButton.Size = New System.Drawing.Size(196, 113)
         Me.ExitButton.TabIndex = 3
         Me.ExitButton.Text = "E&xit"
         Me.ExitButton.UseVisualStyleBackColor = True
         '
         'FirstNameTextBox
         '
-        Me.FirstNameTextBox.Location = New System.Drawing.Point(149, 12)
+        Me.FirstNameTextBox.Location = New System.Drawing.Point(182, 14)
+        Me.FirstNameTextBox.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.FirstNameTextBox.Name = "FirstNameTextBox"
-        Me.FirstNameTextBox.Size = New System.Drawing.Size(250, 26)
+        Me.FirstNameTextBox.Size = New System.Drawing.Size(305, 29)
         Me.FirstNameTextBox.TabIndex = 4
         '
         'LastNameTextBox
         '
-        Me.LastNameTextBox.Location = New System.Drawing.Point(502, 12)
+        Me.LastNameTextBox.Location = New System.Drawing.Point(614, 14)
+        Me.LastNameTextBox.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.LastNameTextBox.Name = "LastNameTextBox"
-        Me.LastNameTextBox.Size = New System.Drawing.Size(250, 26)
+        Me.LastNameTextBox.Size = New System.Drawing.Size(305, 29)
         Me.LastNameTextBox.TabIndex = 5
         '
         'FirstNameLabel
         '
         Me.FirstNameLabel.AutoSize = True
-        Me.FirstNameLabel.Location = New System.Drawing.Point(57, 12)
+        Me.FirstNameLabel.Location = New System.Drawing.Point(70, 14)
+        Me.FirstNameLabel.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.FirstNameLabel.Name = "FirstNameLabel"
-        Me.FirstNameLabel.Size = New System.Drawing.Size(86, 20)
+        Me.FirstNameLabel.Size = New System.Drawing.Size(106, 25)
         Me.FirstNameLabel.TabIndex = 6
         Me.FirstNameLabel.Text = "First Name"
         '
         'LastNameLabel
         '
         Me.LastNameLabel.AutoSize = True
-        Me.LastNameLabel.Location = New System.Drawing.Point(410, 12)
+        Me.LastNameLabel.Location = New System.Drawing.Point(501, 14)
+        Me.LastNameLabel.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LastNameLabel.Name = "LastNameLabel"
-        Me.LastNameLabel.Size = New System.Drawing.Size(86, 20)
+        Me.LastNameLabel.Size = New System.Drawing.Size(106, 25)
         Me.LastNameLabel.TabIndex = 7
         Me.LastNameLabel.Text = "Last Name"
         '
         'ListBoxForm
         '
         Me.AcceptButton = Me.AddButton
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CancelButton = Me.ExitButton
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(978, 540)
         Me.Controls.Add(Me.LastNameLabel)
         Me.Controls.Add(Me.FirstNameLabel)
         Me.Controls.Add(Me.LastNameTextBox)
@@ -118,6 +126,7 @@ Partial Class ListBoxForm
         Me.Controls.Add(Me.ClearButton)
         Me.Controls.Add(Me.AddButton)
         Me.Controls.Add(Me.DataListBox)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "ListBoxForm"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "List Box "
--- ListBoxExample/ListBoxForm.vb
+++ ListBoxExample/ListBoxForm.vb
@@ -19,7 +19,15 @@
     End Sub
 
     Private Sub DataListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DataListBox.SelectedIndexChanged
+        Dim index As Integer
+
         Me.Text = DataListBox.SelectedIndex.ToString
         'TODO add selected to first name and last name text box
+        index = InStr(DataListBox.SelectedItem.ToString, " ")
+
+        FirstNameTextBox.Text = Strings.Left(DataListBox.SelectedItem.ToString, index)
+        'TODO Right() don't work
+        LastNameTextBox.Text = Strings.Right(DataListBox.SelectedItem.ToString, index)
+
     End Sub
 End Class
