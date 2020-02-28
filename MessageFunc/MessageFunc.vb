908e25d8b7b70767bb9017df9d4ca2f3629099d7
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-02-28 08:15:14 -0700

    User message

---
+++ MessageFunc/MessageFunc.vb
@@ -0,0 +1,21 @@
+﻿Module MessageFunc
+
+    Sub Main()
+        'MsgBox(UserMessages(True, "hello", False))
+        Console.WriteLine(UserMessages(True, "hello 1", False))
+        Console.WriteLine(UserMessages(True, "hello 2", False))
+        Console.WriteLine(UserMessages(True, "hello 3", False))
+        MsgBox(UserMessages(True, "", False))
+
+    End Sub
+
+    Function UserMessages(addMessage As Boolean, message As String, clearMessage As Boolean) As String
+        Static formattedMessages As String '= ""
+
+        formattedMessages &= message & vbNewLine
+
+        Return formattedMessages
+
+    End Function
+
+End Module
