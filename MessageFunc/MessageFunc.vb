dd4d82ac4885d1e55537d52192618aa759e0a14c
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-02-29 15:41:47 -0700

    finish unit test

--- MessageFunc/MessageFunc.vb
+++ MessageFunc/MessageFunc.vb
@@ -1,11 +1,10 @@
-﻿Module MessageFunc
+﻿Option Explicit On
+Option Strict On
+Option Compare Binary
+
+Module MessageFunc
 
     Sub Main()
-        'MsgBox(UserMessages(True, "hello", False))
-        'Console.WriteLine(UserMessages(True, "hello 1", False))
-        'Console.WriteLine(UserMessages(True, "hello 2", False))
-        'Console.WriteLine(UserMessages(True, "hello 3", False))
-        'MsgBox(UserMessages(False, "", False))
         TestUserMessages()
         Console.ReadLine()
     End Sub
@@ -13,7 +12,7 @@
     Function UserMessages(addMessage As Boolean, message As String, clearMessage As Boolean) As String
         Static formattedMessages As String
 
-        formattedMessages &= message & vbNewLine
+        'your code here
 
         Return formattedMessages
 
@@ -26,44 +25,47 @@
         Dim seperator As String = StrDup(40, "*") & vbNewLine
 
         For testCounter = 1 To 12
+
             'Initialize
             testString = "Test " & Str(testCounter)
-            control &= testString & vbNewLine
-            saveMessage = True
+            saveMessage = False
             clearMessage = False
 
             'Change Arguments
             If testCounter Mod 3 = 0 Then
                 control = ""
                 clearMessage = True
-            ElseIf testCounter > 5 And testCounter < 8 Then
-                saveMessage = False
+            ElseIf testCounter < 5 Or testCounter > 8 Then
+                saveMessage = True
+                control &= testString & vbNewLine
             End If
 
             'Run Test
+            Console.WriteLine("Testing: " & vbNewLine _
+                    & "Save message flag is:     " & saveMessage & vbNewLine _
+                    & "Message Text is:         " & Chr(34) & testString & Chr(34) & vbNewLine _
+                    & "Clear message flag is:    " & clearMessage)
+
             evaluate = UserMessages(saveMessage, testString, clearMessage)
 
             'Evaluate Test Display Test Results
             If evaluate = control Then
-                Console.WriteLine("Test" & Str(testCounter) & " PASS")
+                Console.WriteLine("Test" & Str(testCounter) _
+                        & "......PASS" & vbNewLine)
                 successCounter += 1
             Else
-                Console.WriteLine(seperator & seperator _
-                                & "Test" & Str(testCounter) & " FAIL" & vbNewLine _
-                                & seperator _
-                                & "Expected: " & vbNewLine & control & vbNewLine _
-                                & seperator _
-                                & "Actual: " & vbNewLine & evaluate)
+                Console.WriteLine("Test" & Str(testCounter) _
+                        & "......FAIL" & vbNewLine & vbNewLine _
+                        & "Expected: " & vbNewLine & control & vbNewLine _
+                        & "Actual: " & vbNewLine & evaluate & vbNewLine)
             End If
+
         Next
 
         'Display Summary
-        Console.WriteLine(seperator & seperator & vbNewLine _
-                          & Str(successCounter) & " of " & Str(testCounter) _
-                          & "tests passed." & vbNewLine & vbNewLine & seperator & seperator)
-
+        Console.WriteLine(vbNewLine & seperator & seperator & vbNewLine _
+                & Str(successCounter) & " of" & Str(testCounter - 1) _
+                & " tests passed" & vbNewLine & vbNewLine & seperator & seperator)
     End Sub
-
-
 
 End Module
