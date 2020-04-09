6330928853c634195d5f154cf1d3be2adc0b11df
Tim Rossiter <rosstimo@isu.edu>
2020-04-09 09:00:21 -0600

    add examples

--- GraphicsExample/GraphicsForm.vb
+++ GraphicsExample/GraphicsForm.vb
@@ -22,6 +22,9 @@
 
     Private Sub GraphicsForm_Click(sender As Object, e As EventArgs) Handles Me.Click
         'drawLine()
+        'DrawEllipse()
+        'DrawRectangle()
+        DrawString()
     End Sub
 
     Private Sub GraphicsForm_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
@@ -33,11 +36,12 @@
         graph = Me.CreateGraphics
 
         Static penColor As Color
+        If penColor.IsEmpty = True Then
+            penColor = Color.Black
+        End If
 
         'constructor for pen object
-        'If penColor Is null Then  ' TODO fix
         Dim myPen As New Pen(penColor)
-        'End If
 
         Static oldX As Integer
         Static oldY As Integer
@@ -46,11 +50,12 @@
             'draw the line
             graph.DrawLine(myPen, oldX, oldY, e.X, e.Y)
         ElseIf e.Button.ToString = "Right" Then
-            graph.Clear(Color.Gray)
+            graph.Clear(Color.LightGray)
+            DrawRectangle(e.X, e.Y)
         ElseIf e.Button.ToString = "Middle" Then
             ColorDialog1.ShowDialog()
-                penColor = ColorDialog1.Color
-                myPen.Color = penColor
+            penColor = ColorDialog1.Color
+            myPen.Color = penColor
         End If
 
         'store position
@@ -62,5 +67,50 @@
         graph.Dispose()
 
     End Sub
+
+    Private Sub GraphicsForm_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
+        Me.Text = e.Button.ToString
+    End Sub
+
+    Sub DrawEllipse()
+        'instantiate graphics object
+        Dim graph As Graphics = Me.CreateGraphics
+        Dim myPen As New Pen(Color.FromArgb(255, 0, 0, 0))
+
+        graph.DrawEllipse(myPen, New Rectangle(50, 50, 100, 100))
+
+        myPen.Dispose()
+        graph.Dispose()
+
+    End Sub
+    Sub DrawRectangle(positionX As Integer, positionY As Integer)
+        'instantiate graphics object
+        Dim graph As Graphics = Me.CreateGraphics
+        Dim myPen As New Pen(Color.FromArgb(255, 0, 255, 0))
+
+        graph.DrawRectangle(myPen, New Rectangle(positionX, positionY, 100, 100))
+
+        myPen.Dispose()
+        graph.Dispose()
+
+    End Sub
+
+    Public Sub DrawString()
+        Dim formGraphics As System.Drawing.Graphics = Me.CreateGraphics()
+        Dim drawString As String = "Sample Text"
+        Dim drawFont As New System.Drawing.Font("Arial", 20)
+        Dim drawBrush As New _
+           System.Drawing.SolidBrush(System.Drawing.Color.Black)
+        Dim x As Single = 150.0
+        Dim y As Single = 50.0
+        Dim drawFormat As New System.Drawing.StringFormat
+        formGraphics.DrawString(drawString, drawFont, drawBrush,
+            x, y, drawFormat)
+
+        drawFont.Dispose()
+        drawBrush.Dispose()
+        formGraphics.Dispose()
+    End Sub
+
 
 End Class
